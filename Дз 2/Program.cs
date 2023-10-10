fc754d8080b931260357a260861cb114145c1155
Artem <koptelovu@gmail.com>
2023-10-11 00:01:31 +0300

    Добавьте файлы проекта.

---
+++ Дз 2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(number / 10 % 10);
+    }
+}
